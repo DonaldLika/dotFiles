a7d18c8f01d7b785a60a3c9c904450e0b13b94a8
Donald Lika <donaldlika@hotmail.com>
2021-09-21 17:34:48 +0200

    Update launch script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,13 +8,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mainbar &
-  done
-else
-  polybar --reload example &
-fi
-
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar &
+done
 
 echo "Polybar launched..."
